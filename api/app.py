33e3eec8f02a2d867d7b7e32bc6f265be35a5d37
Erick-Rangel <rebel863@hotmail.com>
2022-04-19 23:47:41 -0500

    cambios para deploy

--- api/app.py
+++ api/app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 
-from database import setup
-from resources.tasks import tasks_bp
+from database import prueba, setup
 
 app = Flask(__name__)
 setup.create_tables()
 
-app.register_blueprint(tasks_bp)
+@app.route("/prueba", methods=['POST'])
+def add_prueba():
+     Ciudades = request.json['Ciudades']
+     Numero_servicios_diarios = request.json['Numero_servicios_diarios']
+     Meta_para_la_ciudad = request.json['Meta_para_la_ciudad']
+
+     data = (Ciudades,Numero_servicios_diarios,Meta_para_la_ciudad)
+     prueba_id = prueba.insert_prueba(data)
+
+     if prueba_id:
+         prueba = prueba.select_prueba_by_id(prueba_id)
+         return jsonify({'prueba': prueba})
+     return jsonify({'message': 'error in adding prueba'})
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+        app.run(debug=True) 
